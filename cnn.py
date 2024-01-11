3ddf9373ac6e1e36b0642bfc783a735c8f6732e6
runnerboy22 <runnerboy22@gmail.com>
2024-01-10 19:48:50 -0500

    rnn

--- cnn.py
+++ cnn.py
@@ -13,13 +13,19 @@ train_datagen = ImageDataGenerator(
     rescale=1.0 / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True
 )
 training_set = train_datagen.flow_from_directory(
-    "dataset/training_set", target_size=(64, 64), batch_size=32, class_mode="binary"
+    "/Users/tyler/ML/templates/Section 40 - Convolutional Neural Networks (CNN)/dataset/training_set",
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode="binary",
 )
 
 # Preprocessing the Test set
 test_datagen = ImageDataGenerator(rescale=1.0 / 255)
 test_set = test_datagen.flow_from_directory(
-    "dataset/test_set", target_size=(64, 64), batch_size=32, class_mode="binary"
+    "/Users/tyler/ML/templates/Section 40 - Convolutional Neural Networks (CNN)/dataset/test_set",
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode="binary",
 )
 
 # Part 2 - Building the CNN
