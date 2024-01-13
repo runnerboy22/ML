ea33e67a5081da0f2a54931d41e6b76f9c9d7c3f
runnerboy22 <runnerboy22@gmail.com>
2024-01-13 10:26:03 -0800

    starting rnn

--- rnn.py
+++ rnn.py
@@ -1,3 +1,14 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+
+#  import training set google stock price train
+dataset_train = pd.read_csv(
+    "templates/Part 3 - Recurrent Neural Networks 2/Google_Stock_Price_Train.csv"
+)
+training_set = dataset_train.iloc[:, 1:2].values
+
+from sklearn.preprocessing import MinMaxScaler
+
+sc = MinMaxScaler(feature_range=(0, 1))
+training_set_scaled = sc.fit_transform(training_set)
